21534a709af9833c2feddf2fb64f380419d8f3c1
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-08-26 17:15:31 -0300

    desafio um resolvido

--- booleanos.js
+++ booleanos.js
@@ -8,19 +8,19 @@ let stringTrinta = '30'
 let numeroDez = 10
 let stringDez = '10'
 
-if (COMPARAR O numeroUm e a stringUm) {
+if (numeroUm == stringUm) {
   console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
 } else {
   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
 }
 
-if (COMPARAR O numeroTrinta e a stringTrinta) {
+if (numeroTrinta === stringTrinta) {
   console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
 } else {
   console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
 }
 
-if (COMPARAR O numeroDez e a stringDez) {
+if (numeroDez == stringDez) {
   console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
 } else {
   console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
